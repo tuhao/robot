ab601a5c00ed129e1b68ed9f08cd22e2ea9e8579
tuhao <astro.liuhang@gmail.com>
2022-05-24 21:44:11 +0800

    fix

--- src/com/mf/ppm/lh/TrackerTank.java
+++ src/com/mf/ppm/lh/TrackerTank.java
@@ -1,45 +1,86 @@
 package com.mf.ppm.lh;
 
-import robocode.AdvancedRobot;
-import robocode.GunTurnCompleteCondition;
-import robocode.HitWallEvent;
-import robocode.ScannedRobotEvent;
+import robocode.*;
 import robocode.util.Utils;
 
 public class TrackerTank extends AdvancedRobot {
 
-    double moveAngle = Math.PI / 6;
+    double moveAngle = 0D;
 
     double enemyBearing = 0;
 
     double enemyDistance = 1000;
 
-    double safeDistance = 300;
+    double safeDistance = 400;
 
     double firePower = 2;
 
     int dir = 1;
 
-    int continueHit = 0;
+    int continuousHit = 0;
 
     int shot = 0;
 
+    boolean hitWall = false;
+
+    private void moveToCenter() {
+        double x = getX();
+        double y = getY();
+        double deltaX = x - getBattleFieldWidth() / 2;
+        double deltaY = y - getBattleFieldHeight() / 2;
+        double angleToCenter = Math.atan(Math.abs(deltaX / deltaY));
+        if (deltaX >= 0) {
+            if (deltaY >= 0) {
+                turnLeftRadians(Math.PI - angleToCenter + getHeadingRadians());
+            } else {
+                turnLeftRadians(angleToCenter + getHeadingRadians());
+            }
+        } else {
+            if (deltaY >= 0) {
+                turnLeftRadians(Math.PI + angleToCenter + getHeadingRadians());
+            } else {
+                turnLeftRadians(-angleToCenter + getHeadingRadians());
+            }
+        }
+        waitFor(new TurnCompleteCondition(this));
+        setAhead(Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+        waitFor(new MoveCompleteCondition(this));
+    }
+
     private void moveToEnemy() {
-        setTurnRightRadians(enemyBearing + moveAngle);
-        setAhead(50);
-        moveAngle = -moveAngle;
+        if (hitWall) {
+            return;
+        }
+        setTurnRight(enemyBearing);
+        setAhead(100);
     }
 
     private void hideBullet() {
-        setTurnLeftRadians(Math.PI / 2 - enemyBearing);
-        setAhead(100 * dir);
+        if (hitWall) {
+            return;
+        }
+        double delta = 0;
+        if (Math.random() > .618) {
+            delta = 50;
+        }
+        if (enemyBearing >= 90D && enemyBearing < 180D) {
+            setTurnRight(enemyBearing - 90D);
+        } else if (enemyBearing >= -90D && enemyBearing < 0) {
+            setTurnRight(enemyBearing + 90D);
+        } else if (enemyBearing >= 0 && enemyBearing < 90D) {
+            setTurnRight(enemyBearing - 90D);
+        } else if (enemyBearing >= -180D && enemyBearing < -90D) {
+            setTurnRight(enemyBearing + 90D);
+        }
+        setAhead((100 + delta) * dir);
+        waitFor(new MoveCompleteCondition(this));
         dir = -dir;
     }
 
     private void reloadBullet() {
         if (getEnergy() < 10) {
             firePower = getEnergy() / 10;
-        } else if (continueHit >= 3) {
+        } else if (continuousHit >= 3) {
             firePower = 3;
         } else {
             firePower = 2;
@@ -47,25 +88,29 @@ public class TrackerTank extends AdvancedRobot {
     }
 
     public void run() {
-        setTurnLeftRadians(getHeadingRadians() % Math.PI / 2);
+        moveToCenter();
         while (true) {
             if (getRadarTurnRemainingRadians() == 0) {
-                setTurnRadarRightRadians(Math.PI / 4);
+                setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
             }
-//            if (enemyDistance > safeDistance) {
-//                moveToEnemy();
-//            } else {
-//                hideBullet();
-//            }
+            moveBody();
             execute();
+        }
+    }
+
+    private void moveBody() {
+        if (enemyDistance > safeDistance) {
+            moveToEnemy();
+        } else {
+            hideBullet();
         }
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent e) {
         enemyDistance = e.getDistance();
-        enemyBearing = e.getBearingRadians();
-        if (shot > 0) {
+        enemyBearing = e.getBearing();
+        if (shot > 1) {
             shot = 0;
             return;
         }
@@ -84,21 +129,30 @@ public class TrackerTank extends AdvancedRobot {
 
     @Override
     public void onHitWall(HitWallEvent e) {
-        double wallBearing = e.getBearingRadians();
-        if (wallBearing >= -Math.PI / 2 && wallBearing < Math.PI / 2) {
-            setBack(100);
+        hitWall = true;
+        double wallBearing = e.getBearing();
+        if (wallBearing >= -90D && wallBearing < 90D) {
             if (wallBearing < 0) {
-                setTurnRightRadians(Math.PI / 2 + wallBearing);
+                setTurnRight(120D + wallBearing);
             } else {
-                setTurnLeftRadians(Math.PI / 2 - wallBearing);
+                setTurnLeft(120D - wallBearing);
             }
         } else {
-            setAhead(100);
-            if (wallBearing < Math.PI) {
-                setTurnLeftRadians(Math.PI - wallBearing);
+            if (wallBearing < -90D) {
+                setTurnRight(180D + wallBearing);
             } else {
-                setTurnRightRadians(Math.PI + wallBearing);
+                setTurnLeft(180D - wallBearing);
             }
+        }
+        waitFor(new TurnCompleteCondition(this));
+        setAhead(100);
+        hitWall = false;
+    }
+
+    public void onWin(WinEvent e) {
+        for (int i = 0; i < 50; i++) {
+            turnRight(30);
+            turnLeft(30);
         }
     }
 
